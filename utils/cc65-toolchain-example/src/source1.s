3a5568a596ae3d7b9c70fba38cf236aa9caa7fad
ryu10 <ryu10nt@gmail.com>
2025-02-03 22:01:42 -0800

    clean submodule

--- utils/cc65-toolchain-example/src/source1.s
+++
@@ -1,7 +0,0 @@
-.global some_external_var
-
-.segment "CODE"
-
-main:
-    lda some_external_var
-    jmp *
--- utils/cc65-toolchain-example/src/source2.s
+++
@@ -1,7 +0,0 @@
-.include "header1.inc"
-
-.global some_external_var
-
-.segment "CODE"
-
-some_external_var: .byte SOME_CONSTANT
